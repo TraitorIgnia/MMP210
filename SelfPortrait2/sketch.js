1a26217dc581bc542539c0b1b4c6b3fa50225253
TraitorIgnia <42822814+TraitorIgnia@users.noreply.github.com>
2019-03-06 18:39:12 -0500

    Add files via upload

---
+++ SelfPortrait2/sketch.js
@@ -0,0 +1,43 @@
+// self portrait
+function setup() {
+	createCanvas(640, 360);
+}
+
+function draw() {
+	background("skyblue");
+	noStroke();
+	
+	//HEAD
+    stroke(159, 26, 31);
+    fill("black");
+    ellipse(100, 100, 150);
+    fill("black");
+    ellipse(300, 100, 150);
+    fill("white");
+    ellipse(200, 200, 300);
+    
+	// eyes
+	fill("black");
+	ellipse(150, 150, 50, 50);
+	ellipse(250, 150, 50, 50);
+    fill("white");
+	//eyes
+	ellipse(150, 150, 25, 25);
+	ellipse(250, 150, 25, 25);
+	// pupils
+	fill("black");
+	//left
+	ellipse(map(mouseX,0,width,145, 155, true), 150, 15, 15);
+	//right
+	ellipse(map(mouseX,0,width,245, 255, true), 150, 15, 15);
+	//nose
+    fill(0, 0, 0);
+    arc(200, 200, 50, 50, 0, PI);
+    stroke(0, 0, 0);
+    
+    //mouth
+    fill("white");
+    arc(175, 250, 50, 50, 0, PI);
+    arc(225, 250, 50, 50, 0, PI);
+    stroke(0, 0, 0);
+}
